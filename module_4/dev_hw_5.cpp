38e125037089c0033a1b435c24b4b4bdab5ff120
stivencabrera3 <stiven.cabrera3@gmail.com>
2020-10-04 22:09:21 -0400

    complete dev_hw_5

--- module_4/dev_hw_5.cpp
+++ module_4/dev_hw_5.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,64 +15,45 @@ double calculate_distance(double x1, double y1, double x2, double y2)
 
 }
 
-
 int main()
 {
     string line, person1_name, person2_name, x1, y1,x2,y2;
     double d_x1,d_x2,d_y1,d_y2, distance;
     ifstream in_file;
-    in_file.open("dev_hw_5_data.txt");
-    getline(in_file,line);
+    in_file.open("coordinates.txt");
 
-    person1_name = line.substr(0,9);
-    person2_name = line.substr(11,9);
-    x1 = line.substr(22,2);
-    y1 = line.substr(26,2);
-    x2 = line.substr(30,2);
-    y2 = line.substr(34,2);
-    
-
-    d_x1 = stod(x1);
-    d_x2 = stod(x2);
-    d_y1 = stod(y1);
-    d_y2 = stod(y2);
-
-    distance = calculate_distance(d_x1,d_y1,d_x2,d_y2);
-
-    cout << fixed << showpoint << setprecision(3);
-
-    cout << distance << endl;
-
-    if (distance <= 6.0)
+    while(getline(in_file,line))
     {
-        cout << "***CAUTION: SOCIAL DISTANCE VIOLATION***\n"
-            << "VIOLATERS: " << person1_name << ", " <<  person2_name << endl
-            <<  "DISTANCE: " << distance << "ft" << endl;
+        person1_name = line.substr(0,9);
+        person2_name = line.substr(10,9);
+
+        person1_name.erase(remove(person1_name.begin(),person1_name.end(),' '),person1_name.end());
+
+        person2_name.erase(remove(person2_name.begin(),person2_name.end(),' '),person2_name.end());
+
+        x1 = line.substr(20,2);
+        y1 = line.substr(24,2);
+        x2 = line.substr(30,2);
+        y2 = line.substr(34,2);
+        
+        d_x1 = stod(x1);
+        d_x2 = stod(x2);
+        d_y1 = stod(y1);
+        d_y2 = stod(y2);
+
+        distance = calculate_distance(d_x1,d_y1,d_x2,d_y2);
+
+        cout << fixed << showpoint << setprecision(2);
+
+        if (distance <= 6.0)
+        {
+            cout << "***CAUTION: SOCIAL DISTANCE VIOLATION***\n"
+                << "VIOLATORS: " << person1_name << ", " <<  person2_name << endl
+                <<  "DISTANCE: " << distance << "ft" << endl << endl;
+        }
     }
-    else  
-    {
-        cout << "Proper social distancing observed. Carry on, citizens." << endl;
-    }
-
     
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    in_file.close();
 
 
     return 0;
